e6a7bc6c860e801b5e9fc0f1e3e684756d4a9f4c
jennsp <jonas.passweg@bluewin.ch>
2018-02-15 15:25:12 +0100

    Update SensorMonitor.sol

--- SensorMonitor.sol
+++ SensorMonitor.sol
@@ -1,24 +1,93 @@
-contract EnergySensor {
-    //get Measurements map from 
-    public getMeasurements() return mapping {}
+pragma solidity ^0.4.20;
+
+contract Grid {
+    //A Map
+    mapping (address => uint256) production;
+    mapping (address => uint256) consumption;
+    mapping (address => address) sensorOwner;
+    
+    function setOwnerOfSensor(address sensor) public {
+        sensorOwner[sensor] = msg.sender;
+    }
+    
+    function getSensorOwner(address sensor) public view returns (address) {
+        return sensorOwner[sensor];
+    }
+    
+    function setConsumption(address user, uint256 consumption) public {
+        consumption[user] = consumption;
+    }
+    
+    function getConsumption(address user) public view returns (uint256) {
+        return consumption[user];
+    }
+    
+    function setProduction(address user, uint256 consumption) public {
+        production[user] = production;
+    }
+    
+    function getProduction(address user) public view returns (uint256) {
+        return production[user];
+    }
+    
+    function authenticate() public view returns (address) {
+        address owner = getSensorOwner(msg.sender);
+        require(owner != 0);
+        return owner;
+    }  
+}
+
+contract SensorOwner {
+    mapping (address => uint8) authenticatedSensors;
+    address SensorAuthenticator;
+    
+    function SensorOwner() {
+        Grid userMaps = new Grid();
+        OurEnergySensor sensorMonitor = new OurEnergySensor();
+        User userFunctions = new User();
+        Authenticator auth = new Authenticator();
+        authenticator.setSensorOwner(this);
+    }
+    
+    function addSensor(address newSensor) public {
+        require(msg.sender == SensorAuthenticator); 
+        authenticatedSensors[newSensor] = 1;
+    }
     
 }
 
-contract OurEnergySensor is EnergySensor {
-    public isOurEnergySensor() {
-        return true;
+contract Authenticator {
+    SensorOwner owner;
+    function isAuthenticated() public view returns (bool) {
+        return (owner.authenticatedSensors[msg.sender] == 1);
     }
     
-    public getPrediction() return (uint128 prediction) {}
+    function setSensorOwner(SensorOwner owner) {
+        this.owner = owner;
+    };
+}
+
+contract OurEnergySensor {
+
+    Grid userMaps;
     
-    //scans address
-    //if address != NULL draw power
-    public authenticate() {
-        
+    function OurEnergySensor() {
+        SensorAuthenticator = msg.sender;
+        userMaps = new Grid();
     }
     
     //sends measurements on blockchain
-    //sensors need to authenticate
-    //
-    public sendMeasurements() {}
+    //sensor is calling that
+    function sendMeasurement(uint256 production, uint256 consumption) public {
+        isOurEnergySensor();
+        address owner = authenticate();
+        userMaps.setConsumption(owner,consumption);
+        userMaps.setProduction(owner,production);
+    }
+}
+
+contract User {
+    function getMyConsuption() {}
+    function getMyProduction() {}
+    
 }
